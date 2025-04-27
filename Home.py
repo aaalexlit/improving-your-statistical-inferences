32005d07c4cde9902e913c5d0e995a91fb6e342d
Alex Litvinov <aaalex.lit@gmail.com>
2025-04-27 17:23:55 +0200

    move the page into pages dir

--- Home.py
+++ Home.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(page_title="Improving Statistical Inferences", page_icon="📈", layout="wide")
+
 st.title("Improving Your Statistical Inferences 📈")
 st.write("""
 Welcome to the interactive app for exploring statistical simulations and concepts!
