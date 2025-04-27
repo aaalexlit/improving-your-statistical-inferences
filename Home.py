8e71fdba597144ccf807dd4f55bf87c1cfbc8502
Alex Litvinov <aaalex.lit@gmail.com>
2025-04-27 17:27:06 +0200

    explicitly add sidebar to home

--- Home.py
+++ Home.py
@@ -2,13 +2,16 @@ import streamlit as st
 
 st.set_page_config(page_title="Improving Statistical Inferences", page_icon="📈", layout="wide")
 
-st.title("Improving Your Statistical Inferences 📈")
+st.title("📈 Improving Your Statistical Inferences")
+
+with st.sidebar:
+    st.header("Welcome! 👋")
+    st.write("Use the sidebar to navigate between simulations and analyses.")
+
+# Main body content
 st.write("""
-Welcome to the interactive app for exploring statistical simulations and concepts!
+Welcome to the interactive app for exploring statistical concepts!
 
-Navigate using the sidebar to explore:
-- P-Value Simulation
-- (More features coming soon...)
-
----
+- Go to **P-Value Simulation** to simulate experiments
+- (More features will be added soon!)
 """)
